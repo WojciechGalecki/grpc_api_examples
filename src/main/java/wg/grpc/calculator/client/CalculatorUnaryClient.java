90e2f4224992e05ce2885904e0b3e75f171ab76c
WojciechGalecki <wojciech.galecki@wp.pl>
2019-12-28 11:00:10 +0100

    added logger for calculator service

--- src/main/java/wg/grpc/calculator/client/CalculatorUnaryClient.java
+++ src/main/java/wg/grpc/calculator/client/CalculatorUnaryClient.java
@@ -1,5 +1,7 @@
 package wg.grpc.calculator.client;
 
+import java.util.logging.Logger;
+
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import io.grpc.StatusRuntimeException;
@@ -9,9 +11,13 @@ import com.proto.calculator.SquareRootRequest;
 import com.proto.calculator.SquareRootResponse;
 
 public class CalculatorUnaryClient {
+    private static final Logger log = Logger.getLogger(CalculatorUnaryClient.class.getName());
 
     public static void main(String[] args) {
+        getSquareRoot();
+    }
 
+    private static void getSquareRoot() {
         ManagedChannel channel = ManagedChannelBuilder.forAddress("localhost", 50052)
             // disable SSL/TLS - for local development
             .usePlaintext()
@@ -27,9 +33,9 @@ public class CalculatorUnaryClient {
                 .setNumber(number)
                 .build());
 
-            System.out.println("Square root: " + response.getNumberRoot());
+            log.info("Square root: " + response.getNumberRoot());
         } catch (StatusRuntimeException e) {
-            System.out.println("ERROR:");
+            log.warning("ERROR: ");
             e.printStackTrace();
         }
 
--- src/main/java/wg/grpc/calculator/server/CalculatorServer.java
+++ src/main/java/wg/grpc/calculator/server/CalculatorServer.java
@@ -1,21 +1,26 @@
 package wg.grpc.calculator.server;
 
 import java.io.IOException;
+import java.util.logging.Logger;
 
 import io.grpc.Server;
 import io.grpc.ServerBuilder;
 
 public class CalculatorServer {
+    private static final Logger log = Logger.getLogger(CalculatorServer.class.getName());
 
     public static void main(String[] args) throws IOException, InterruptedException {
+        runServer();
+    }
 
+    private static void runServer() throws InterruptedException, IOException {
         Server server = ServerBuilder.forPort(50052)
             .addService(new CalculatorService())
             .build();
 
         server.start();
 
-        System.out.println("Server is running...");
+        log.info("Server is running...");
 
         Runtime.getRuntime().addShutdownHook(new Thread(server::shutdown));
 
